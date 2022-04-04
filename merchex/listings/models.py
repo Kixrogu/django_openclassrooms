3e5e015df29e6a9b6b482e8eef82a8a78d4fbd97
Mathieu Thauvin <mathieu.thauvin@alur3.com>
2022-04-04 15:47:48 +0200

    Fin de journée 04/04 : fond bleu dégueulasse

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -3,5 +3,6 @@ from django.db import models
 class Band(models.Model):
     name = models.fields.CharField(max_length=100)
 
+
 class Artist(models.Model):
     name = models.fields.CharField(max_length=100)
